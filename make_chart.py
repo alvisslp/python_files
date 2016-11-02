8e35fcdcdcfef96835f3d161de467c376cf7035a
Julian COLIN <julian.colin@murex.com>
2016-11-02 16:36:58 +0100

    improve coding style of make_chart

--- make_chart.py
+++ make_chart.py
@@ -8,7 +8,10 @@ import plotly.plotly as py
 import plotly.graph_objs as go
 
 def usage():
-    print("non")
+    """
+    print usage
+    """
+    print('non')
 
 def main():
     """
@@ -17,7 +20,7 @@ def main():
 
     try:
         opts, _ = getopt.getopt(sys.argv[1:], "hf:")
-    except getopt.GetoptError as err:
+    except getopt.GetoptError as _:
         usage()
         sys.exit(2)
 
@@ -43,53 +46,76 @@ class Graph(object):
         self.cpu_y = []
         self.memory_y = []
         self.load_y = []
+        self.figure = None
 
     def make_graph(self, file_name):
+        """
+        create graph
+        """
         graph_f = open(file_name, 'r')
-        x = 0
+        x_val = 0
         for index, line in enumerate(graph_f):
             if index % 4 == 0:
                 # This is the time
                 self.time_x.append(x)
-                x += 30
+                x_val += 30
             elif index % 4 == 1:
                 # This is the CPU
-                self.cpu_y.append(self.get_cpu(line))
+                self.cpu_y.append(get_cpu(line))
             elif index % 4 == 2:
                 # This is the memory
                 self.memory_y.append(self.get_memory(line))
             elif index % 4 == 3:
-                self.load_y.append(self.get_load(line))
+                self.load_y.append(get_load(line))
 
-        cpu_trace = self.get_trace(self.time_x, self.cpu_y, "CPU", "red")
-        memory_trace = self.get_trace(self.time_x, self.memory_y, "MEMORY", "blue")
-        load_trace = self.get_trace(self.time_x, self.load_y, "load average", "grean")
+        cpu_trace = get_trace(self.time_x, self.cpu_y, "CPU", "red")
+        memory_trace = get_trace(self.time_x, self.memory_y, "MEMORY",
+                                      "blue")
+        load_trace = get_trace(self.time_x, self.load_y, "load average",
+                                    "grean")
 
         data = go.Data([cpu_trace, memory_trace, load_trace])
-        layout = go.Layout(title = "CPU and MEMORY consumption and load average",
-                           xaxis = {'title' : 'time (s)'},
-                           yaxis = {'title' : 'CPU and Memory load average'})
-        self.figure = go.Figure(data = data, layout = layout)
+        layout = go.Layout(title="CPU, MEMORY consumption and load average",
+                           xaxis={'title' : 'time (s)'},
+                           yaxis={'title' : 'CPU and Memory load average'})
+        self.figure = go.Figure(data=data, layout=layout)
 
     def publish(self):
-        py.plot(self.figure, filename = 'Server consumption', auto_open = False)
+        """
+        public results on plot.ly
+        """
+        py.plot(self.figure, filename='Server consumption', auto_open=False)
 
-    def get_trace(self, x, y, name, color):
-        return go.Scatter(x = x, y = y,
-                          line = dict(color = color),
-                          mode = "lines", name = name)
+    def get_trace(x_axis, y_axis, name, color):
+        """
+        get the trace
+        """
+        return go.Scatter(x=x_axis, y=y_axis, line=dict(color=color),
+                          mode="lines", name=name)
 
-    def get_cpu(self, line):
+    def get_cpu(line):
+        """
+        get the cpu
+        """
         return 100.0 - float(line.split()[2].split('%')[0])
 
-    def get_load(self, line):
+    def get_load(line):
+        """
+        get the load average
+        """
         return float(line.split(',')[0].split()[-1])
 
-    def get_memory(self, line):
+    def get_memory(line):
+        """
+        get the memory
+        """
         spl = line.split()
-        return self.get_mem_value(spl)
+        return get_mem_value(spl)
 
-    def get_mem_value(self, mem):
+    def get_mem_value(mem):
+        """
+        get memory value
+        """
         total = int(mem[1].split('G')[0])
         if mem[4][-1] == 'M':
             used = float(mem[4].split('M')[0]) / 1024.0
@@ -102,4 +128,4 @@ class Graph(object):
         return total - used
 
 if __name__ == "__main__":
-        main()
+    main()
